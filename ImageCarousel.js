a2fc501cfb1278d30e77cedef1843cd59fab4a54
jamesdebnam <62795194+jamesdebnam@users.noreply.github.com>
2020-04-16 14:28:28 +0100

    Added functionality to carousel
    Still requires a standard frame size, and transitions...

--- ImageCarousel.js
+++ ImageCarousel.js
@@ -8,3 +8,24 @@ let numToWord = {
 let picNum = 1
 let visiblePicture = document.querySelector(`#${numToWord[picNum]}`)
 visiblePicture.hidden= false
+
+let leftArrow = document.querySelector('#left')
+let rightArrow = document.querySelector('#right')
+
+leftArrow.addEventListener('click', () => {
+  if (picNum==1) {
+    picNum=5;
+  } else {picNum--}
+  visiblePicture.hidden=true
+  visiblePicture = document.querySelector(`#${numToWord[picNum]}`)
+  visiblePicture.hidden= false
+})
+
+rightArrow.addEventListener('click', () => {
+  if (picNum==5) {
+    picNum=1;
+  } else {picNum++}
+  visiblePicture.hidden=true
+  visiblePicture = document.querySelector(`#${numToWord[picNum]}`)
+  visiblePicture.hidden= false
+})
