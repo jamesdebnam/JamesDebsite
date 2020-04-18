4ceb5b4939e4f08608c4f8e68a9f640886d6b5d9
jamesdebnam <62795194+jamesdebnam@users.noreply.github.com>
2020-04-18 13:45:04 +0100

    Reworked carousel to allow sliding
    Having some trouble with css positioning.... Also have decided to use same size photos to make the process a bit easier

--- ImageCarousel.js
+++ ImageCarousel.js
@@ -1,31 +1,44 @@
-let numToWord = {
-  1:'one',
-  2:'two',
-  3:'three',
-  4:'four',
-  5:'five'
-}
-let picNum = 1
-let visiblePicture = document.querySelector(`#${numToWord[picNum]}`)
-visiblePicture.hidden= false
+// let numToWord = {
+//   1:'one',
+//   2:'two',
+//   3:'three',
+//   4:'four',
+//   5:'five'
+// }
+// let picNum = 1
+// let visiblePicture = document.querySelector(`#${numToWord[picNum]}`)
+// visiblePicture.hidden= false
+//
+// let leftArrow = document.querySelector('#left')
+// let rightArrow = document.querySelector('#right')
+//
+// leftArrow.addEventListener('click', () => {
+//   if (picNum==1) {
+//     picNum=5;
+//   } else {picNum--}
+//   visiblePicture.hidden=true
+//   visiblePicture = document.querySelector(`#${numToWord[picNum]}`)
+//   visiblePicture.hidden= false
+// })
+//
+// rightArrow.addEventListener('click', () => {
+//   if (picNum==5) {
+//     picNum=1;
+//   } else {picNum++}
+//   visiblePicture.hidden=true
+//   visiblePicture = document.querySelector(`#${numToWord[picNum]}`)
+//   visiblePicture.hidden= false
+// })
+const carousel_track = document.querySelector('.carousel')
+const panes = Array.from(carousel_track.children)
 
-let leftArrow = document.querySelector('#left')
-let rightArrow = document.querySelector('#right')
+let spaceBefore = 0
+panes.forEach((slide, index) => {
+    let slideWidth = slide.getBoundingClientRect().width;
+    if (index > 0) {
+        slide.style.left = slideWidth 
 
-leftArrow.addEventListener('click', () => {
-  if (picNum==1) {
-    picNum=5;
-  } else {picNum--}
-  visiblePicture.hidden=true
-  visiblePicture = document.querySelector(`#${numToWord[picNum]}`)
-  visiblePicture.hidden= false
-})
-
-rightArrow.addEventListener('click', () => {
-  if (picNum==5) {
-    picNum=1;
-  } else {picNum++}
-  visiblePicture.hidden=true
-  visiblePicture = document.querySelector(`#${numToWord[picNum]}`)
-  visiblePicture.hidden= false
-})
+    }
+    
+    
+});
