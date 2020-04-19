fc664093bc39627d9f4d7062adcd847ac0c3de60
jamesdebnam <62795194+jamesdebnam@users.noreply.github.com>
2020-04-19 11:47:55 +0100

    Added keyboard carousel functionality

--- ImageCarousel.js
+++ ImageCarousel.js
@@ -1,14 +1,22 @@
 
-let leftArrow = document.querySelector('#left')
-let rightArrow = document.querySelector('#right')
+const leftArrow = document.querySelector('#left');
+const rightArrow = document.querySelector('#right');
 const carousel = document.querySelector('.carousel-list');
 const slides = Array.from(carousel.children);
-const carouselPic = document.querySelector('.carousel-pic-only')
+const carouselPic = document.querySelector('.carousel-pic-only');
+const indicators = document.querySelector('.indicators');
+const indicatorDots = Array.from(indicators.children);
+
 let slideWidth = slides[0].getBoundingClientRect().width;
 
 for (let i = 0; i < slides.length; i++) {
     slides[i].style.left = slideWidth * i + "px";
 }
+
+let slideNumber = 0;
+dotColor();
+
+// resizes the size of each slide when the page is resized
 window.addEventListener('resize', ()=> {
     slideWidth = slides[0].getBoundingClientRect().width;
     for (let i = 0; i < slides.length; i++) {
@@ -16,16 +24,33 @@ window.addEventListener('resize', ()=> {
     }
 });
 
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode == '37'){
+        slideLeft();
+    } else if(e.keyCode == '39'){
+        slideRight();
+    }
+});
 
-let slideNumber = 0
+leftArrow.addEventListener('click', slideLeft);
 
-leftArrow.addEventListener('click', () => {
-    slideNumber = (slideNumber == 0) ? 8 : slideNumber-1;
-    carouselPic.style.transform = `translateX(-${slideWidth*slideNumber}px)`
-  
-})
+rightArrow.addEventListener('click', slideRight);
 
-rightArrow.addEventListener('click', () => {
+function dotColor() {
+    for (dot of indicatorDots) {
+        dot.style.backgroundColor = '#667c8d';
+    }
+    indicatorDots[slideNumber].style.backgroundColor = '#17222c'
+}
+
+function slideLeft() {
+    slideNumber = (slideNumber == 0) ? 8 : slideNumber - 1;
+    carouselPic.style.transform = `translateX(-${slideWidth * slideNumber}px)`;
+    dotColor();
+}
+
+function slideRight() {
     slideNumber = (slideNumber == 8) ? 0 : slideNumber + 1;
-    carouselPic.style.transform = `translateX(-${slideWidth*slideNumber}px)`
-})
+    carouselPic.style.transform = `translateX(-${slideWidth * slideNumber}px)`;
+    dotColor();    
+}
